f54c99d9e2a8ec49a919c236ab88a0d94f24c19f
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2024-08-17 05:46:31 +0700

    update: memoirs page

--- src/app/memoirs/[filename]/page.tsx
+++ src/app/memoirs/[filename]/page.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import Head from "next/head";
 
 async function getMemoirContent(filename: string) {
   // const url = process.env.NEXT_PUBLIC_API_LOCAL;
@@ -34,15 +35,38 @@ export default async function MemoirPage({
 }: {
   params: { filename: string };
 }) {
+  const url = process.env.NEXT_PUBLIC_API_BASE_URL;
   const memoir = await getMemoirContent(params.filename);
+  const postTitle = memoir.title || "Memoir";
+  const postDescription = memoir.content.slice(0, 150) || "Memoir description";
+  const postUrl = `${url}/memoirs/${params.filename}`;
+  const postImage = "/icon.png";
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">{memoir.title}</h1>
-      <div className="prose prose-xl max-w-none space-y-4">
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>
-          {memoir.content}
-        </ReactMarkdown>
+    <>
+      <Head>
+        <title>{postTitle}</title>
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="theme-color" content="#0f1e36" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={postImage} />
+        <meta name="twitter:site" content="@CaamVilvact" />
+        <meta name="viewport" content="width=375" />
+        <meta property="og:url" content={postUrl} />
+        <meta property="og:type" content="article" />
+        <meta name="description" content={postDescription} />
+        <meta property="og:description" content={postDescription} />
+        <meta property="og:title" content={postTitle} />
+        <meta property="og:image" content={postImage} />
+      </Head>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">{memoir.title}</h1>
+        <div className="prose prose-xl max-w-none space-y-4">
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {memoir.content}
+          </ReactMarkdown>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
