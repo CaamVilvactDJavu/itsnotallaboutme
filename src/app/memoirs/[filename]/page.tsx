b9076379a33511b31b43adfb30405c2c5ac90eeb
CaamVilvactDJavu <caam.vilvact11@gmail.com>
2024-08-27 16:17:23 +0700

    update: memoirs page

--- src/app/memoirs/[filename]/page.tsx
+++ src/app/memoirs/[filename]/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { memoirs } from "../../../data/memoirsList";
@@ -8,16 +11,18 @@ async function getMemoirContent(filename: string) {
   const url = process.env.NEXT_PUBLIC_API_BASE_URL;
 
   try {
-    const response = await fetch(`${url}/api/memoirs/${filename}`);
+    const response = await fetch(`${url}/api/memoirs/${filename}`, {
+      next: { revalidate: 60 },
+    });
 
     if (!response.ok) {
-      throw new Error(`Failed to fetch note: ${response.statusText}`);
+      throw new Error(`Failed to fetch memoir: ${response.statusText}`);
     }
 
     const memoir = await response.json();
     return memoir;
   } catch (error) {
-    console.error("Error fetching note content:", error);
+    console.error("Error fetching memoir content:", error);
     return { title: "Error", content: "Could not load content." };
   }
 }
@@ -26,12 +31,30 @@ export async function generateStaticParams() {
   return memoirs.map((memoir) => ({ filename: memoir }));
 }
 
-export default async function MemoirPage({
+export const revalidate = 60;
+
+export default function MemoirPage({
   params,
 }: {
   params: { filename: string };
 }) {
-  const memoir = await getMemoirContent(params.filename);
+  const [memoir, setMemoir] = useState({ title: "", content: "" });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchMemoir() {
+      setIsLoading(true);
+      const fetchedMemoir = await getMemoirContent(params.filename);
+      setMemoir(fetchedMemoir);
+      setIsLoading(false);
+    }
+    fetchMemoir();
+  }, [params.filename]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   const postTitle = memoir.title || "Memoir";
   const postDescription = memoir.content.slice(0, 150) || "Memoir description";
   const postUrl = `https://itsnotallaboutme.shuttleapp.rs/memoirs/${params.filename}`;
@@ -46,6 +69,7 @@ export default async function MemoirPage({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={postImage} />
         <meta name="twitter:site" content="@CaamVilvact" />
+        <meta name="twitter:url" content={postUrl} />
         <meta name="twitter:description" content={postDescription} />
         <meta name="viewport" content="width=375" />
         <meta property="og:url" content={postUrl} />
